66be91059f03921a6f24b46fb37c15d887218504
RattleSN4K3 <RattleSN4K3@gmail.com>
2015-02-21 19:32:17 +0100

    Initial repo/plugin

---
+++ Source/SafeSpawn/Private/SafeSpawnPlugin.cpp
@@ -0,0 +1,25 @@
+//#include "SafeSpawnMutator.h"
+
+#include "Core.h"
+#include "Engine.h"
+#include "ModuleManager.h"
+#include "ModuleInterface.h"
+
+class FSafeSpawnPlugin : public IModuleInterface
+{
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+
+IMPLEMENT_MODULE( FSafeSpawnPlugin, SafeSpawn )
+
+void FSafeSpawnPlugin::StartupModule()
+{
+	
+}
+
+void FSafeSpawnPlugin::ShutdownModule()
+{
+	
+}
---
+++ Source/SafeSpawn/SafeSpawn.Build.cs
@@ -0,0 +1,20 @@
+namespace UnrealBuildTool.Rules
+{
+	public class SafeSpawn : ModuleRules
+	{
+		public SafeSpawn(TargetInfo Target)
+		{
+			PrivateIncludePaths.Add("SafeSpawn/Private");
+
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"Core",
+					"CoreUObject",
+					"Engine",
+					"UnrealTournament",
+				}
+			);
+		}
+	}
+}
